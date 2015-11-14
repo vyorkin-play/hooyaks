3b460bd9da00a7e030449761ea416e15d5f96fe0
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-14 16:06:16 +0300

    add webpack config, template, deps to counter example

---
+++ examples/counter/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'hooyaks-react';
+import App from './containers/App';
+import { createStore } from './store';
---
+++ examples/counter/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: __dirname,
+        exlucde: /node_module/
+      }
+    ]
+  },
+  devtool: 'cheap-module-inline-source-map',
+}
