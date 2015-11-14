b6738029255030a690323e5ac59a1020ccf44bc6
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-14 20:49:39 +0300

    misc fixes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import mapObj from 'map-obj';
-import * as utils from './utils.js';
-
+import * as utils from './utils';
 
 export default {
   create: (reducer, initialState) => {
     let state = initialState;
+
     const dispatch = (action) => {
       state = reducer(state, action);
       return action;
     };
+
     dispatch({ type: '.i./INIT' });
+
     return { dispatch, getState: () => state };
   },
 
-  combine: (reducers) => (state = {}, action) =>
+  combine: reducers => (state = {}, action) =>
     mapObj(reducers, (key, reducer) => [key, reducer(state[key], action)]),
 
   ...utils
